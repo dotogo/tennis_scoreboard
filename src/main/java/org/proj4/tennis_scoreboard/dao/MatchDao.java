b6e1e2134485258eb95a84cf5bbe3350b896b2a1
Dmitriy Kolesnik <13na18@gmail.com>
2025-07-04 17:32:06 +0300

    feat: add getAllMatches() and findByPlayer() to MatchDao

--- src/main/java/org/proj4/tennis_scoreboard/dao/MatchDao.java
+++ src/main/java/org/proj4/tennis_scoreboard/dao/MatchDao.java
@@ -1,11 +1,15 @@
 package org.proj4.tennis_scoreboard.dao;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import org.proj4.tennis_scoreboard.entity.Match;
+import org.proj4.tennis_scoreboard.entity.Player;
 import org.proj4.tennis_scoreboard.util.HibernateUtil;
 
-public class MatchDao {
+import java.util.List;
 
+public class MatchDao {
     public Match save(Match match) {
 
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
@@ -19,5 +23,62 @@ public class MatchDao {
             e.printStackTrace();
         }
         return match;
+    }
+
+    public List<Match> getAllMatches(int page, int pageSize) {
+        Transaction transaction = null;
+        List<Match> matches = List.of();
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+
+            transaction = session.beginTransaction();
+
+            int offset = (page - 1) * pageSize;
+            Query<Match> query = session.createQuery("from Match order by id", Match.class);
+            query.setFirstResult(offset);
+            query.setMaxResults(pageSize);
+
+            matches = query.getResultList();
+
+            transaction.commit();
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+
+                // TODO make custom exception
+                throw new RuntimeException("Error while getting all matches", e);
+            }
+        }
+        return matches;
+    }
+
+    public List<Match> findByPlayer(Player player, int page, int pageSize) {
+        Transaction transaction = null;
+        List<Match> matches = List.of();
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+
+            int offset = (page - 1) * pageSize;
+            String hql = "from Match where firstPlayer = :player or secondPlayer = :player order by id";
+            Query<Match> query = session.createQuery(hql, Match.class);
+            query.setParameter("player", player);
+            query.setFirstResult(offset);
+            query.setMaxResults(pageSize);
+
+            matches = query.getResultList();
+
+            transaction.commit();
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+
+                // TODO make custom exception
+                throw new RuntimeException("Error while finding matches by player", e);
+            }
+        }
+        return matches;
     }
 }
---
+++ src/main/java/org/proj4/tennis_scoreboard/servlet/MatchesServlet.java
@@ -0,0 +1,38 @@
+package org.proj4.tennis_scoreboard.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Map;
+
+@WebServlet("/matches")
+public class MatchesServlet extends BaseServlet {
+    private static final String PARAM_PAGE = "page";
+    private static final String PARAM_NAME_FILTER = "filter_by_player_name";
+
+    private static final String REQUIRED_PARAMETERS_MISSING = "One or more parameters have invalid names or are missing. " +
+                                                              "Required parameters: \"%s\", \"%s\"".formatted(PARAM_PAGE, PARAM_NAME_FILTER);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, String[]> parameterMap = req.getParameterMap();
+
+        if (!parameterMap.containsKey(PARAM_PAGE) || !parameterMap.containsKey(PARAM_NAME_FILTER)) {
+            sendErrorResponse(resp, HttpServletResponse.SC_BAD_REQUEST, REQUIRED_PARAMETERS_MISSING);
+            return;
+        }
+
+        String page = parameterMap.get(PARAM_PAGE)[0];
+        String playerName = parameterMap.get(PARAM_NAME_FILTER)[0];
+
+        // TODO make page validation
+        Integer pageValue = Integer.valueOf(page.trim());
+
+        if (page.trim().isEmpty()) {
+            pageValue = 1;
+        }
+    }
+}
