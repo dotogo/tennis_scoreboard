507abea472e6547465d40e68b59e97af77a90f48
Dmitriy Kolesnik <13na18@gmail.com>
2026-02-14 17:10:13 +0200

    replace 2 classes of filters with 1 class EncodingFilter

---
+++ src/main/java/org/proj4/tennis_scoreboard/filter/EncodingFilter.java
@@ -0,0 +1,25 @@
+package org.proj4.tennis_scoreboard.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter(value = {
+        "/new-match", "/matches", "/match-score"
+})
+public class EncodingFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        req.setCharacterEncoding("UTF-8");
+        res.setCharacterEncoding("UTF-8");
+
+        res.setContentType("application/json");
+
+        super.doFilter(req, res, chain);
+    }
+}
