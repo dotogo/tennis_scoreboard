47c5c2a4bfe855457ecbc4709759296fb097b6a4
Dmitriy Kolesnik <13na18@gmail.com>
2026-03-01 19:05:30 +0200

    fix: program crashes when quickly click the mouse at the moment of victory

--- src/main/java/org/proj4/tennis_scoreboard/service/OngoingMatchesService.java
+++ src/main/java/org/proj4/tennis_scoreboard/service/OngoingMatchesService.java
@@ -4,19 +4,15 @@ import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import org.proj4.tennis_scoreboard.entity.OngoingMatch;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public class OngoingMatchesService {
     private static final Cache<UUID, OngoingMatch> ongoingMatches = Caffeine.newBuilder().build();
 
-    public OngoingMatch getMatch(UUID id) {
+    public Optional<OngoingMatch> getMatch(UUID id) {
         OngoingMatch ongoingMatch = ongoingMatches.getIfPresent(id);
-
-        if (ongoingMatch != null) {
-            return ongoingMatch;
-        }
-        //TODO make custom exception
-        throw new RuntimeException("ongoingMatch is null");
+        return Optional.ofNullable(ongoingMatch);
     }
 
     public UUID addMatch(OngoingMatch ongoingMatch) {
--- src/main/java/org/proj4/tennis_scoreboard/servlet/MatchScoreServlet.java
+++ src/main/java/org/proj4/tennis_scoreboard/servlet/MatchScoreServlet.java
@@ -11,6 +11,7 @@ import org.proj4.tennis_scoreboard.service.MatchScoreCalculationService;
 import org.proj4.tennis_scoreboard.service.OngoingMatchesService;
 
 import java.io.IOException;
+import java.util.Optional;
 import java.util.UUID;
 
 @WebServlet("/match-score")
@@ -19,40 +20,48 @@ public class MatchScoreServlet extends BaseServlet {
     private static final String ATTR_MATCH = "match";
     private static final String PARAM_POINT_WINNER = "point-winner";
     private static final String INVALID_UUID = "Invalid UUID";
+    private static final String ERROR_GETTING_MATCH = "Something went wrong while getting ongoing match";
 
     private final OngoingMatchesService ongoingMatchesService = new OngoingMatchesService();
     private final MatchScoreCalculationService matchScoreCalculationService = new MatchScoreCalculationService();
     private final FinishedMatchesPersistenceService finishedMatchesPersistenceService = new FinishedMatchesPersistenceService();
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         String uuidFromParameter = req.getParameter(PARAM_UUID);
+        checkUuid(resp, uuidFromParameter);
         UUID uuid = UUID.fromString(uuidFromParameter);
 
-        OngoingMatch match = ongoingMatchesService.getMatch(uuid);
-        if (match == null) {
-            System.out.println("Match not found");
-        } else {
-            System.out.println("Match found");
-        }
-        req.setAttribute(ATTR_MATCH, match);
+        Optional<OngoingMatch> matchOptional = ongoingMatchesService.getMatch(uuid);
 
-//        resp.getWriter().write(uuid.toString());
-        req.getRequestDispatcher("/WEB-INF/views/match-score.jsp").forward(req, resp);
+        if (matchOptional.isPresent()) {
+            req.setAttribute(ATTR_MATCH, matchOptional.get());
+            req.getRequestDispatcher("/WEB-INF/views/match-score.jsp").forward(req, resp);
+        }
+
+        if (matchOptional.isEmpty()) {
+            sendErrorResponse(resp, ERROR_GETTING_MATCH);
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String uuidFromParameter = req.getParameter(PARAM_UUID);
+        checkUuid(resp, uuidFromParameter);
+        UUID uuid = UUID.fromString(uuidFromParameter);
 
-        try {
-            UUID.fromString(uuidFromParameter);
-        } catch (IllegalArgumentException e) {
-            sendErrorResponse(resp, INVALID_UUID);
+        Optional<OngoingMatch> matchOptional = ongoingMatchesService.getMatch(uuid);
+
+        OngoingMatch match = null;
+
+        if (matchOptional.isPresent()) {
+            match = matchOptional.get();
         }
 
-        UUID uuid = UUID.fromString(uuidFromParameter);
-        OngoingMatch match = ongoingMatchesService.getMatch(uuid);
+        if (match == null) {
+            resp.sendRedirect("/matches");
+            return;
+        }
 
         String pointWinner = req.getParameter(PARAM_POINT_WINNER);
 
@@ -67,12 +76,18 @@ public class MatchScoreServlet extends BaseServlet {
 
             req.setAttribute(ATTR_MATCH, match);
             req.getRequestDispatcher("/WEB-INF/views/final-score.jsp").forward(req, resp);
-
-//            resp.getWriter().write("Match finished");
             return;
         }
 
         req.setAttribute(ATTR_MATCH, match);
         req.getRequestDispatcher("/WEB-INF/views/match-score.jsp").forward(req, resp);
+    }
+
+    private void checkUuid(HttpServletResponse resp, String uuidFromParameter) throws IOException {
+        try {
+            UUID.fromString(uuidFromParameter);
+        } catch (IllegalArgumentException e) {
+            sendErrorResponse(resp, INVALID_UUID);
+        }
     }
 }
