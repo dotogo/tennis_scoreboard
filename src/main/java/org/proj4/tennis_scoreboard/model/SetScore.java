cc597a1e723e8fa5a904494c4f8c81f5c0c922af
Dmitriy Kolesnik <13na18@gmail.com>
2026-06-26 18:39:02 +0300

    refactor: add SetScore as part of score model redesign

---
+++ src/main/java/org/proj4/tennis_scoreboard/model/SetScore.java
@@ -0,0 +1,115 @@
+package org.proj4.tennis_scoreboard.model;
+
+public class SetScore implements TennisScore {
+    private int firstPlayerGames;
+    private int secondPlayerGames;
+    private boolean setFinished;
+
+    private TennisScore currentGame;
+
+    public SetScore() {
+        currentGame = new RegularGameScore();
+    }
+
+    @Override
+    public void addPointFirstPlayer() {
+        if (GameStatus.ONGOING.equals(currentGame.getStatus())) {
+            currentGame.addPointFirstPlayer();
+        }
+
+        int gamesBeforeCalculateNewValue = firstPlayerGames;
+
+        firstPlayerGames = calculatePlayerGames(GameStatus.FIRST_PLAYER_WON, firstPlayerGames);
+
+        finishSetIfPlayerWon(GameStatus.FIRST_PLAYER_WON, firstPlayerGames, secondPlayerGames);
+
+        updateCurrentGameIfNewStarting(firstPlayerGames, gamesBeforeCalculateNewValue);
+    }
+
+    @Override
+    public void addPointSecondPlayer() {
+        if (GameStatus.ONGOING.equals(currentGame.getStatus())) {
+            currentGame.addPointSecondPlayer();
+        }
+
+        int gamesBeforeCalculateNewValue = secondPlayerGames;
+
+        secondPlayerGames = calculatePlayerGames(GameStatus.SECOND_PLAYER_WON, secondPlayerGames);
+
+        finishSetIfPlayerWon(GameStatus.SECOND_PLAYER_WON, secondPlayerGames, firstPlayerGames);
+
+        updateCurrentGameIfNewStarting(secondPlayerGames, gamesBeforeCalculateNewValue);
+    }
+
+    @Override
+    public GameStatus getStatus() {
+        if (setFinished) {
+            if (isRegularGame() && firstPlayerGames > secondPlayerGames) {
+                return GameStatus.FIRST_PLAYER_WON;
+            }
+
+            if (isRegularGame() && secondPlayerGames > firstPlayerGames) {
+                return GameStatus.SECOND_PLAYER_WON;
+            }
+
+            if (isTieBreak() && GameStatus.FIRST_PLAYER_WON.equals(currentGame.getStatus())) {
+                return GameStatus.FIRST_PLAYER_WON;
+            }
+
+            if (isTieBreak() && GameStatus.SECOND_PLAYER_WON.equals(currentGame.getStatus())) {
+                return GameStatus.SECOND_PLAYER_WON;
+            }
+        }
+        return GameStatus.ONGOING;
+    }
+
+    private void finishSetIfPlayerWon(GameStatus gameStatus, int winnerPoints, int opponentPoints) {
+        if (isRegularGame() && isSetFinishedOnRegularGame(winnerPoints, opponentPoints)) {
+            setFinished = true;
+        }
+
+        if (isTieBreak() && gameStatus.equals(currentGame.getStatus())) {
+            setFinished = true;
+        }
+    }
+
+    private int calculatePlayerGames(GameStatus gameStatus, int pointWinnerGames) {
+        if (isRegularGame() && gameStatus.equals(currentGame.getStatus())) {
+            return ++pointWinnerGames;
+        }
+        return pointWinnerGames;
+    }
+
+    private void updateCurrentGameIfNewStarting(int updatedGamesScore, int oldGamesScore) {
+        if (isTieBreakStarting()) {
+            currentGame = new TieBreakScore();
+        }
+
+        if (!setFinished && isNewRegularGameStarting(updatedGamesScore, oldGamesScore)) {
+            currentGame = new RegularGameScore();
+        }
+    }
+
+    private boolean isTieBreakStarting() {
+        if (isRegularGame()) {
+            return firstPlayerGames == 6 && secondPlayerGames == 6;
+        }
+        return false;
+    }
+
+    private boolean isNewRegularGameStarting(int updatedGamesScore, int oldGamesScore) {
+        return isRegularGame() && (updatedGamesScore - oldGamesScore == 1);
+    }
+
+    private boolean isSetFinishedOnRegularGame(int winnerPoints, int opponentPoints) {
+        return winnerPoints >= 6 && (winnerPoints - opponentPoints) >= 2;
+    }
+
+    private boolean isRegularGame() {
+        return currentGame instanceof RegularGameScore;
+    }
+
+    private boolean isTieBreak() {
+        return currentGame instanceof TieBreakScore;
+    }
+}
